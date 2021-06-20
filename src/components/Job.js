e78b8eceb299d5f5c98d7622483e91212cb5918d
ArzuAshik <arzu2050@gmail.com>
2021-06-21 02:13:50 +0600

    homepage design done

---
+++ src/components/Job.js
@@ -0,0 +1,33 @@
+import React, { useContext } from 'react';
+import { useToasts } from 'react-toast-notifications';
+import { UserContext } from '../App';
+
+const Job = ({job}) => {
+    const {_id, tag, company_name, price, description} = job;
+    // eslint-disable-next-line
+    const [userInfo, setUserInfo] = useContext(UserContext);
+    const { addToast } = useToasts();
+    
+    function handleApply(jobID){
+        if(userInfo.userID){
+            console.log(jobID);
+        }else{
+            console.log("jj");
+            addToast("Please Login To Apply.", {
+                appearance: 'warning',
+                autoDismiss: true,
+              });
+        }
+    }
+    return (
+        <div className="job">
+            <h2>{tag}</h2>
+            <p>Company Name: {company_name}</p>
+            <p>Price: {price}</p>
+            <p>{description}</p>
+            <button onClick={() => handleApply(_id)} className="apply">Apply for Job</button>
+        </div>
+    );
+};
+
+export default Job;
--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,9 +1,69 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Job from '../../components/Job';
 
 const HomePage = () => {
+    const [allJobs, setAllJobs] = useState([]);
+    const [jobsToShow, setJobsToShow] = useState([]);
+    const [currentPage, setCurrentPage] = useState(1);
+
+    useEffect(() => {
+        fetch("https://find-job-server.herokuapp.com/all-jobs")
+        .then(res => res.json())
+        .then(result => {
+            setAllJobs(result);
+            setJobsToShow(result);
+        });
+    }, []);
+
+    function handleFilter(filterBy){
+        if(filterBy === "0"){
+            setJobsToShow(allJobs);
+        }else{
+            const toShowFirst = [];
+            const toShowLast = [];
+            allJobs.forEach(j => {
+                if(j.tag === filterBy){
+                    toShowFirst.push(j);
+                }else{
+                    toShowLast.push(j);
+                }
+            });
+            setJobsToShow([...toShowFirst, ...toShowLast]);
+        }
+    }
+
+    function pagination(){
+        const totalPage = jobsToShow.length / 20;
+        const buttons = [];
+        if(totalPage > 1){
+            for(let i = 1; i <= totalPage; i++){
+                const btn = (<button key={i} onClick={() => setCurrentPage(i)} className={currentPage === i ? "pagination-btn active" : "pagination-btn"} >{i}</button>);
+                buttons.push(btn);
+            }
+        }
+        return buttons;
+    }
+
     return (
-        <div>
-            Home
+        <div className="page home-page">
+            <div className="filter">
+                <select name="filter" id="filter" onChange={(e) => handleFilter(e.target.value)} >
+                    <option value="0">Default</option>
+                    <option value="Web Development">Web Development</option>
+                    <option value="Graphics Design">Graphics Design</option>
+                    <option value="SEO">SEO</option>
+                    <option value="Wordpress Design">Wordpress Design</option>
+                    <option value="Data Entry">Data Entry</option>
+                </select>
+            </div>
+            {
+                jobsToShow.slice((20 * currentPage) - 20, currentPage * 20).map(job => <Job key={job._id} job={job}/>)
+            }
+            <div className="pagination">
+                {
+                    pagination()
+                }
+            </div>
         </div>
     );
 };
